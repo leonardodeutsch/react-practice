8829ab9e0c9740250cbed96d6bccab7343cfd103
Leo <deutschleonardo@gmail.com>
2022-06-29 16:07:31 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {  }
+  render() { 
+    return ( 
+       <h1>Practice</h1>
+     );
+  }
+}
+ 
+export default App;
+
