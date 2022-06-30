470f310419482ad09734c24d69a535055e6c4339
Leo <deutschleonardo@gmail.com>
2022-06-30 15:53:11 -0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MovieCard from './MovieCard.jsx';
 
+const API_URL = 'http://www.omdbapi.com?apikey=43ff0ff4';
+import SearchIcon from './search.svg';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = (title) => {
+    axios.get(`${API_URL}&s=${title}`)
+    .then(res => {
+      console.log(res.data.Search)
+      setMovies(res.data.Search);
+    })
   }
-  state = {  }
-  render() { 
-    return ( 
-       <h1>Practice</h1>
-     );
+
+  useEffect(() => {
+    searchMovies('');
+  }, []);
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      searchMovies(searchTerm);
+    }
   }
+
+  return (
+    <div className="app">
+
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies && <div className="container">
+        {movies.map((movie) => (
+          <MovieCard movie1={movie}/>
+        ))}
+      </div>}
+    </div> 
+  );
 }
  
 export default App;
