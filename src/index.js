e9041e5885d2d5a04987eb2198b8a71c2ae886b4
Leo <deutschleonardo@gmail.com>
2022-07-01 11:14:27 -0500

    deleted comments

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import App from "./App.jsx";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
 
-
-// var mountNode = document.getElementById("app");
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 )
-// ReactDOM.render(<App />, mountNode);
